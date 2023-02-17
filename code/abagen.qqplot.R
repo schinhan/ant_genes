dc0450a04e74be43ac0ea39f1206f11a2cd718a7
pjawinski <77676981+pjawinski@users.noreply.github.com>
2023-02-17 14:55:58 +0100

    inital commit

---
+++ code/abagen.qqplot.R
@@ -0,0 +1,117 @@
+#!/usr/bin/env Rscript
+
+# ===========================
+# === draw manhattan plot ===
+# ===========================
+
+# get arguments
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)!=10) {
+  stop(paste0('expected 10 arguments, but ', length(args), ' arguments provided.'), call.=FALSE)
+}
+
+# get arguments from command line
+abagenFile = args[1] # abagenFile='results/perm.pvals.txt'
+targetDir = args[2] # targetDir="results/qqplot/"
+prune = args[3] # prune = TRUE
+drawCI = args[4] # drawCI = FALSE
+xend = as.numeric(args[5]) # xend = 6
+xsteps = as.numeric(args[6]) # xsteps = 2
+yend = as.numeric(args[7]) # yend = 8
+ysteps = as.numeric(args[8]) # ysteps = 2
+width = as.numeric(args[9]) # width = 3
+height = as.numeric(args[10]) # height = 4
+
+message(paste0('\n--- qq-plot settings ---',
+               '\nabagenFile: ', abagenFile,
+               '\ntargetDir: ', targetDir,
+               '\nprune: ', prune,
+               '\ndrawCI: ', drawCI,
+               '\nxend: ', xend,
+               '\nxsteps: ', xsteps,
+               '\nyend: ', yend,
+               '\nysteps: ', ysteps,
+               '\nwidth: ', width,
+               '\nheight: ', height,'\n'))
+
+# attach required packages
+for (pkg in c('data.table','dplyr','ggplot2', 'stringr')) { eval(bquote(suppressWarnings(suppressPackageStartupMessages(require(.(pkg)))))) }
+
+# create folder
+system(paste0('mkdir -p ', targetDir))
+
+# import data
+message(paste0('[1/2] Importing data.'))
+df = read.delim(abagenFile, sep='\t', header=T, stringsAsFactors=FALSE)
+
+# create qq-plot function
+# credits to Kamil Slowikowski (https://slowkow.com/notes/ggplot2-qqplot/)
+gg_qqplot = function(pvals, ci = 0.90, drawCI = TRUE, truncateLim = Inf, pointshape = 16, pointsize = 1, pointcolor = "#0072BD", pointalpha = 1, prune = FALSE) {
+  n  = length(pvals)
+  df = data.frame(
+    observed = -log10(sort(pvals)),
+    expected = -log10(ppoints(n)),
+    clower   = -log10(qbeta(p = (1 - ci) / 2, shape1 = 1:n, shape2 = n:1)),
+    cupper   = -log10(qbeta(p = (1 + ci) / 2, shape1 = 1:n, shape2 = n:1))
+  )
+  
+  # remove overlapping points (pvals > 0.01)
+  if (prune == TRUE) {
+  df$expected[df$expected<2] = round(df$expected[df$expected<2],2)
+  df = df[-which(df$expected<2 & duplicated(df$expected)),]
+  }
+  
+  # Truncate
+  df$observed[df$observed > truncateLim] = truncateLim
+
+  # set axis labels
+  log10Pe = expression("Expected -log"[10]*"("*italic(p)*")")
+  log10Po = expression("Observed -log"[10]*"("*italic(p)*")")
+
+  # draw confidence intervals, or don't.
+  if (drawCI == TRUE) {
+  qqplot = ggplot(df) +
+    geom_ribbon(
+      mapping = aes(x = expected, ymin = clower, ymax = cupper),
+      alpha = 0.1)
+  } else {
+  qqplot = ggplot(df)
+  }
+  
+  # draw qq-plot
+  qqplot +
+    geom_point(aes(expected, observed), shape = pointshape, size = pointsize, colour = pointcolor, alpha = pointalpha) + #, shape = 1, size = 3
+    geom_abline(intercept = 0, slope = 1, alpha = 1, color = "black", linetype="solid", size = 0.25) + # geom_segment(aes(x = 0, xend = max(expected), y = 0, yend = max(expected)), alpha = 0.5) +
+    xlab(log10Pe) +
+    ylab(log10Po)
+}
+
+# create plot
+message(paste0('[1/2] Creating qq plot.'))
+pCols = names(df)[grep('p\\.',names(df))]
+traits = str_replace(pCols, 'p.', '')
+
+for (i in 1:length(traits)) {
+  tmp = gg_qqplot(df[[pCols[i]]], prune = T, drawCI = drawCI, truncateLim = yend) +
+      geom_segment(aes(x=0,xend=xend,y=-Inf,yend=-Inf), colour = "black", size = 0.25) +
+      geom_segment(aes(y=0,yend=yend,x=-Inf,xend=-Inf), colour = "black", size = 0.25) +
+      theme_bw() +
+          scale_x_continuous(expand = expansion(mult = c(0.03,0), add = c(0,0)), limits = c(0,xend), breaks = seq(0,xend,xsteps)) +
+          scale_y_continuous(expand = expansion(mult = c(0,0), add = c(0,0.5)), limits = c(0,yend), breaks = seq(0,yend,ysteps)) +
+          theme(plot.margin=unit(c(0.25,0.75,0,0),"cm"),
+                panel.border = element_blank(),
+                axis.line = element_blank(),
+                axis.ticks = element_line(colour = "black", size = 0.25),
+                axis.ticks.length=unit(.15, "cm"),
+                panel.grid.major.x = element_blank(),
+                panel.grid.minor.x = element_blank(),
+                panel.grid.major.y = element_blank(),
+                panel.grid.minor.y = element_blank(),
+                axis.title.y = element_text(size = 16, margin = margin(t = 0, r = 5, b = 0, l = 5)),
+                axis.title.x = element_text(size = 16, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+                axis.text.x = element_text(angle = 0, size = 12, vjust = 0.5, margin = margin(t = 3, r = 0, b = 0, l = 0)),
+                axis.text.y = element_text(angle = 0, size = 12, vjust = 0.5, margin = margin(t = 0, r = 3, b = 0, l = 0)))
+  ggsave(paste0(targetDir, '/', traits[i], '.qqplot.png'), tmp, width = width, height = height, units = 'in', dpi = 300)
+  system(paste0('chmod -R 770 ', targetDir, '/', traits[i], '.qqplot.png'))
+}
+message(paste0('--- qq-plot completed ---\n'))
