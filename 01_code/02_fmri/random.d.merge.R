3535d37a034cca9592a5b027fd9334e5161d4d66
schinhan <125668187+schinhan@users.noreply.github.com>
2023-05-23 14:17:08 +0200

    Add files via upload

--- 01_code/02_fmri/random.d.merge.R
+++ 01_code/02_fmri/random.d.merge.R
@@ -7,7 +7,7 @@
 setwd('/.../02_data/03_fMRI')
 
 # attach required packages to current R session
-for (pkg in c('doParallel','data.table','bigmemory')) { eval(bquote(suppressPackageStartupMessages(require(.(pkg))))) }
+for (pkg in c('dplyr','data.table')) { eval(bquote(suppressPackageStartupMessages(require(.(pkg))))) }
 
 # load observed correlations
 alert.cor = read.delim('alert.obs.corr.txt', sep = '\t', header = T)
@@ -18,17 +18,17 @@ control.cor = read.delim('control.obs.corr.txt', sep = '\t', header = T)
 alert.p = as.vector(unlist(read.table('alert.pval.twotailed.txt')))
 orient.p = as.vector(unlist(read.table('orient.pval.twotailed.txt'))) 
 control.p = as.vector(unlist(read.table('control.pval.twotailed.txt')))
-pvals = data.frame(gene = alert.corr$genes, alert.p = alert.p, control.p = control.p, orient.p = orient.p) 
+pvals = data.frame(gene = alert.cor$genes, alert.p = alert.p, control.p = control.p, orient.p = orient.p) 
 
 # merge with pvals
 df = data.frame(gene = alert.cor$genes,
   alert.p = alert.p, control.p = control.p, orient.p = orient.p,
   alert.cor = alert.cor$cor.estimate, control.cor = control.cor$cor.estimate, orient.cor = orient.cor$cor.estimate) 
 
-# calculate t-values
-df$alert.t = qnorm(df$alert.p)*-sign(df$alert.cor)
-df$control.t = qnorm(df$control.p)*-sign(df$control.cor)
-df$orient.t = qnorm(df$orient.p)*-sign(df$orient.cor)
+# calculate z-values
+df$alert.z = qnorm(df$alert.p)*-sign(df$alert.cor)
+df$control.z = qnorm(df$control.p)*-sign(df$control.cor)
+df$orient.z = qnorm(df$orient.p)*-sign(df$orient.cor)
 
 # ======================
 # === get some stats ===
@@ -38,7 +38,10 @@ df$orient.t = qnorm(df$orient.p)*-sign(df$orient.cor)
 sapply(data.frame(df[,c('alert.p','control.p','orient.p')]<0.05),sum)
 
 # how many genes are FDR-significant?
-sapply(data.frame(sapply(data.frame(df[,c('alert.p','control.p','orient.p')]),p.adjust,method='BH')<0.05),sum)
+df$alert.fdr = p.adjust(df$alert.p,method='BH')
+df$control.fdr = p.adjust(df$control.p,method='BH')
+df$orient.fdr = p.adjust(df$orient.p,method='BH')
+sapply(data.frame(df[,c('alert.fdr','control.fdr','orient.fdr')]<0.05),sum)
 
 # how many genes are Bonferroni-significant? caveat: number of permutations must be larger than 1/(0.05/nrow(results))
 sapply(data.frame(df[,c('alert.p','control.p','orient.p')]<(0.05/nrow(df))),sum)
@@ -49,8 +52,8 @@ sapply(data.frame(df[,c('alert.p','control.p','orient.p')]),min)
 # how many unique p-values
 sapply(sapply(data.frame(df[,c('alert.p','control.p','orient.p')]),unique),length)
 
-# how many unique t-values
-sapply(sapply(data.frame(df[,c('alert.t','control.t','orient.t')]),unique),length)
+# how many unique z-values
+sapply(sapply(data.frame(df[,c('alert.z','control.z','orient.z')]),unique),length)
 
 # save results
 save(df, file = 'perm.results.RData')
@@ -58,5 +61,47 @@ write.table(df, file = 'perm.results.txt', sep = '\t', quote = F, col.names = T,
 
 # save pvals
 pvals = df[,c('gene','alert.p','control.p','orient.p')]
-save(pvals, file = '04_perm.pvals.RData')
-write.table(pvals, file = '04_perm.pvals.txt', sep = '\t', quote = F, col.names = T, row.names = F)
+save(pvals, file = 'perm.pvals.RData')
+write.table(pvals, file = 'results/perm.pvals.txt', sep = '\t', quote = F, col.names = T, row.names = F)
+
+# ========================================
+# === save gene results for supplement ===
+# ========================================
+
+# get gene description from prepared RefSeq file)
+refseq = data.frame(fread(cmd=paste0("gzip -dc .../03_data_visualization/01_fMRI/Manhattan_QQ_Plots/GRCh37_latest_genomic.edit.gff.gz"), sep='\t', header=F, stringsAsFactors=FALSE))
+
+# get info
+message('Querying gene infos.')
+info = data.frame(matrix(NA, nrow = nrow(df), ncol = 6))
+names(info) = c('gene','chr','start','stop','symbol','description')
+pb = txtProgressBar(min = 1, max = nrow(info), style = 3)
+for (i in 1:nrow(df)) {
+  setTxtProgressBar(pb, i)
+  info$gene[i] = df$gene[i]
+  tmp = refseq[grep(paste0("\\b",df$gene[i],"\\b"),refseq$V10),]
+  if (nrow(tmp) == 0) { tmp = refseq[grep(paste0("\\b",df$gene[i],"\\b"),refseq$V13),] }
+  if (nrow(tmp) == 0) { tmp = refseq[grep(paste0("\\b",df$gene[i],"\\b"),refseq$V14),] }
+  if (nrow(tmp) > 0) {
+    info$chr[i] = tmp$V9[1]
+    info$start[i] = tmp$V4[1]
+    info$stop[i] = tmp$V5[1]
+    info$symbol[i] = tmp$V10[1]
+    info$description[i] = tmp$V12[1]
+  }
+}
+
+# merge refseq and df
+df = left_join(df, info, by = 'gene')
+
+# create output
+df$top.corAbs =  df[,grep('.cor',names(df))] %>% abs() %>% apply(1, FUN = max)
+df$top.p = df[,grep('[.]p',names(df))] %>% apply(1, FUN = min)
+df$top.fdr = df[,grep('[.]fdr',names(df))] %>% apply(1, FUN = min)
+df$`NA` = ''
+output = df[,c('gene','chr','start','stop','description','top.corAbs','top.p','top.fdr',
+    'NA','alert.cor','alert.z','alert.p','alert.fdr',
+    'NA','control.cor','control.z','control.p','control.fdr',
+    'NA','orient.cor','orient.z','orient.p','orient.fdr')]
+output = output[order(output$top.fdr,output$top.p,-output$top.corAbs),]
+write.table(output, file = 'perm.results.suppl.txt', sep = '\t', quote = F, col.names = T, row.names = F)
